f160f1ebe38017d9cd7ff18e69642ec1a7e1681f
Renzo Lacovara <rnlacovara@gmail.com>
2022-10-25 19:09:03 -0300

    tailwind

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -5,14 +5,15 @@ function Header(prop) {
   let nombre1 = prop.nombre1;
   let nombre2 = prop.nombre2;
   return (
-    <header className="bg-secundario h-28 w-full p-3 flex grow-0">
-      <nav className="h-full w-full flex justify-between">
-        <div className="h-full w-full flex items-center gap-5">
-          <h1 className="text-2xl">
-            Bienvenido a <span className="font-font1 font-semibold">{nombre1}</span>
+    <header className="bg-secundario h-28 w-full flex">
+      <nav className="h-full w-full flex justify-between p-3">
+        <div className="h-full w-full flex items-center gap-2">
+          <h1 className="text-2xl pt-2">
+            Bienvenido a{" "}
+            <span className="font-font1 font-semibold">{nombre1}</span>
             <span className="font-font2 font-bold">{nombre2}</span>
           </h1>
-          <img className="h-10 w-10" src="castle1.png" alt="logo" />
+          <i class="fa-solid fa-broom-ball text-3xl text-detalle"></i>
         </div>
         <CartWidget valor="5" />
       </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,18 @@ module.exports = {
   darkMode: "class", // or 'media' or 'class'
   theme: {
     colors: {
-      'princ' : "var(--color-principal)",
-      'principald': "var(--color-principal-d)",
-      'secundario': "var(--color-secundario)",
-      'secundariod': "var(--color-secundario-d)",
-      'detalle': "var(--color-detalle)",
+      princ: "var(--color-principal)",
+      principald: "var(--color-principal-d)",
+      secundario: "var(--color-secundario)",
+      secundariod: "var(--color-secundario-d)",
+      detalle: "var(--color-detalle)",
     },
     fontFamily: {
-      'font1': ["Harry Potter", "sans-serif"],
-      'font2': ["Amazon Ember Display", "sans-serif"],
-      'font3': ["Red Hat Display", "sans-serif"],
+      font1: ["Harry Potter", "sans-serif"],
+      font2: ["Amazon Ember Display", "sans-serif"],
+      font3: ["Red Hat Display", "sans-serif"],
     },
-    extend: {
-     
-    },
+    extend: {},
   },
   variants: {
     extend: {},
