4b722fe9a868c1762c05032ea007eb3b1929a0b6
Renzo Lacovara <rnlacovara@gmail.com>
2022-11-09 15:27:18 -0300

    condicionales

--- src/componentes/Item.js
+++ src/componentes/Item.js
@@ -4,50 +4,38 @@ import { Link } from "react-router-dom";
 const Item = ({ nombre, imagen, precio, tienda, id, oferta, ofertaValor }) => {
   return (
     <div>
-      {oferta === "si" ? (
-        <article className="my-8 w-64 rounded-2xl shadow-lg h-96 flex flex-col items-center justify-between">
-          <div className="text-center">
-            <h1>{nombre}</h1>
-            <h4 className="inline line-through text-sm">${precio}</h4>
-            <h3 className="inline text-detalle font-bold"> ${ofertaValor}</h3>
-            <div>
-              <img
-                className="rounded-lg h-60 w-56 object-contain bg-mapa"
-                src={imagen}
-                alt={nombre}
-              />
-            </div>
-            <h4 className="text-sm">by {tienda.nombre}</h4>
-          </div>
-
-          <Link
-            to={"/Item/" + id}
-            className="bg-secundario rounded-md px-2 py-1 my-4 hover:bg-detalle hover:text-princ"
+      <article className="my-8 w-64 rounded-2xl shadow-lg h-96 flex flex-col items-center justify-between">
+        <div className="text-center">
+          <h1>{nombre}</h1>
+          <h4
+            className={`inline ${
+              oferta === "si"
+                ? "line-through text-sm"
+                : "text-detalle font-bold"
+            }`}
           >
-            Ver mas
-          </Link>
-        </article>
-      ) : (
-        <article className="w-64 rounded-2xl shadow-lg h-96 flex flex-col items-center justify-between">
-          <div className="text-center">
-            <h1>{nombre}</h1>
-            <h3 className="text-detalle font-bold">${precio}</h3>
+            ${precio}
+          </h4>
+          {oferta === "si" && (
+            <h3 className="inline text-detalle font-bold"> ${ofertaValor}</h3>
+          )}
+          <div>
             <img
-              className="rounded-lg h-60 w-56  object-contain bg-mapa"
+              className="rounded-lg h-60 w-56 object-contain bg-mapa"
               src={imagen}
               alt={nombre}
             />
-            <h4 className="text-sm">by {tienda.nombre}</h4>
           </div>
+          <h4 className="text-sm">by {tienda.nombre}</h4>
+        </div>
 
-          <Link
-            to={"/Item/" + id}
-            className="bg-secundario rounded-md px-2 py-1 my-4 hover:bg-detalle hover:text-princ"
-          >
-            Ver mas
-          </Link>
-        </article>
-      )}
+        <Link
+          to={"/Item/" + id}
+          className="bg-secundario rounded-md px-2 py-1 my-4 hover:bg-detalle hover:text-princ"
+        >
+          Ver mas
+        </Link>
+      </article>
     </div>
   );
 };
--- src/componentes/ItemDetail.js
+++ src/componentes/ItemDetail.js
@@ -13,65 +13,45 @@ const ItemDetail = ({ detalle }) => {
 
   const handleOnAdd = (cantidad) => {};
 
-  if (detalles === undefined) {
-    return <h1 className="h-screen">Cargando...</h1>;
-  } else if (detalles.oferta === "si") {
-    return (
-      <div className="h-full my-14 w-screen flex justify-center items-center">
-        <div className="rounded-2xl shadow-lg overflow-hidden flex w-2/3 h-full">
-          <img
-            className="h-96 w-2/5 object-contain bg-mapa bg-cover overflow-hidden"
-            src={detalles.imagen}
-            alt={detalles.nombre}
-          />
+  return detalles === undefined ? (
+    <h1 className="h-screen">Cargando...</h1>
+  ) : (
+    <div className="h-full my-14 w-screen flex justify-center items-center">
+      <div className="rounded-2xl shadow-lg overflow-hidden flex w-2/3 h-full">
+        <img
+          className="h-96 w-2/5 object-contain bg-mapa bg-cover overflow-hidden"
+          src={detalles.imagen}
+          alt={detalles.nombre}
+        />
 
-          <div className=" w-3/5 flex flex-col justify-between">
-            <div className="text-center h-full">
-              <h2 className="text-2xl inline-block">
-                {detalles.nombre} -{" "}
-                <h2 className="inline-block text-sm line-through">
-                  ${detalles.precio}
-                </h2>
+        <div className=" w-3/5 flex flex-col justify-between">
+          <div className="text-center h-full">
+            <h2 className="text-2xl inline-block">
+              {detalles.nombre} -{" "}
+              <h2
+                className={`inline-block ${
+                  detalles.oferta === "si"
+                    ? "text-sm line-through"
+                    : "text-detalle font-bold"
+                }`}
+              >
+                ${detalles.precio}
+              </h2>
+              {detalles.oferta === "si" && (
                 <h2 className="inline-block text-detalle font-bold ml-2">
                   ${detalles.ofertaValor}
                 </h2>
-              </h2>
-              <p className="text-justify p-6 text-sm">{detalles.descripcion}</p>
-            </div>
-            <div>
-              <ItemCount handleOnAdd={handleOnAdd} />
-            </div>
+              )}
+            </h2>
+            <p className="text-justify p-6 text-sm">{detalles.descripcion}</p>
+          </div>
+          <div>
+            <ItemCount handleOnAdd={handleOnAdd} />
           </div>
         </div>
       </div>
-    );
-  } else {
-    return (
-      <div className="h-full my-14 w-screen flex justify-center items-center">
-        <div className="rounded-2xl shadow-lg overflow-hidden flex w-2/3 h-full">
-          <img
-            className="h-96 w-2/5 object-contain bg-mapa bg-cover overflow-hidden"
-            src={detalles.imagen}
-            alt={detalles.nombre}
-          />
-          <div className="w-3/5 flex flex-col justify-between">
-            <div className="text-center">
-              <h2 className="text-2xl inline-block">
-                {detalles.nombre} -{" "}
-                <h2 className="inline-block text-detalle font-bold">
-                  ${detalles.precio}
-                </h2>
-              </h2>
-              <p className="text-justify p-6 text-sm">{detalles.descripcion}</p>
-            </div>
-            <div>
-              <ItemCount handleOnAdd={handleOnAdd} />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default ItemDetail;
